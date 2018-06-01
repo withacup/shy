f06ded3be65196a875e11f7c9632b59ebb29ac2e
Hanyu Sun <hanyusun@hotmail.com>
2018-05-31 23:07:47 -0700

    Add transfer client

--- transfer/transferclient.c
+++ transfer/transferclient.c
@@ -81,4 +81,63 @@ int main(int argc, char **argv)
     }
 
     /* Socket Code Here */
+    int client_socket;
+    ssize_t len;
+    struct sockaddr_in remote_addr;
+    char buffer[BUFSIZE];
+    int file_size;
+    FILE *received_file;
+    int remain_data = 0;
+
+    /* Zeroing remote_addr struct */
+    memset(&remote_addr, 0, sizeof(remote_addr));
+
+    /* Construct remote_addr struct */
+    remote_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, hostname, &(remote_addr.sin_addr));
+    remote_addr.sin_port = htons(portno);
+
+    /* Create client socket */
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        fprintf(stderr, "Error creating socket --> %s\n", strerror(errno));
+
+        exit(EXIT_FAILURE);
+    }
+
+    /* Connect to the server */
+    if (connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) == -1)
+    {
+        fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
+
+        exit(EXIT_FAILURE);
+    }
+
+    /* Receiving file size */
+    recv(client_socket, buffer, BUFSIZE, 0);
+    file_size = atoi(buffer);
+    //fprintf(stdout, "\nFile size : %d\n", file_size);
+
+    received_file = fopen(filename, "w");
+    if (received_file == NULL)
+    {
+        fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+
+        exit(EXIT_FAILURE);
+    }
+
+    remain_data = file_size;
+
+    while (((len = recv(client_socket, buffer, BUFSIZE, 0)) > 0) && (remain_data > 0))
+    {
+        fwrite(buffer, sizeof(char), len, received_file);
+        remain_data -= len;
+        fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
+    }
+    fclose(received_file);
+
+    close(client_socket);
+
+    return 0;
 }
